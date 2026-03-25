6e54e7f692f76c2615e9d8f4d945ee1fdb0408c4
Ferdinand Hartmann <ferdinand.hartmann@web.de>
2026-03-25 16:12:52 +0900

    added wandb entity hardcode fetchrobot (wandb team)

--- scripts/rsl_rl/train.py
+++ scripts/rsl_rl/train.py
@@ -122,6 +122,8 @@ def main(env_cfg: ManagerBasedRLEnvCfg | DirectRLEnvCfg | DirectMARLEnvCfg, agen
     """Train with RSL-RL agent."""
     # override configurations with non-hydra CLI arguments
     agent_cfg = cli_args.update_rsl_rl_cfg(agent_cfg, args_cli)
+    if agent_cfg.logger == "wandb":
+        os.environ.setdefault("WANDB_ENTITY", "fetchrobot")
     env_cfg.scene.num_envs = args_cli.num_envs if args_cli.num_envs is not None else env_cfg.scene.num_envs
     agent_cfg.max_iterations = (
         args_cli.max_iterations if args_cli.max_iterations is not None else agent_cfg.max_iterations
