de6e7f48105750dfc5e4ff5b6d6f139933ae8fb5
Ferdinand Hartmann <ferdinand.hartmann@web.de>
2026-03-28 20:17:15 +0900

    made clear vel 3l and 4l (legs) task

--- source/unitree_rl_lab/unitree_rl_lab/tasks/locomotion/robots/go2/__init__.py
+++ source/unitree_rl_lab/unitree_rl_lab/tasks/locomotion/robots/go2/__init__.py
@@ -1,12 +1,23 @@
 import gymnasium as gym
 
 gym.register(
-    id="Unitree-Go2-Velocity",
+    id="Unitree-Go2-Velocity-3L",
     entry_point="isaaclab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": f"{__name__}.velocity_env_cfg:RobotEnvCfg",
-        "play_env_cfg_entry_point": f"{__name__}.velocity_env_cfg:RobotPlayEnvCfg",
+        "env_cfg_entry_point": f"{__name__}.velocity_3l_env_cfg:RobotEnvCfg",
+        "play_env_cfg_entry_point": f"{__name__}.velocity_3l_env_cfg:RobotPlayEnvCfg",
+        "rsl_rl_cfg_entry_point": f"unitree_rl_lab.tasks.locomotion.agents.rsl_rl_ppo_cfg:BasePPORunnerCfg",
+    },
+)
+
+gym.register(
+    id="Unitree-Go2-Velocity-4L",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.velocity_4l_env_cfg:RobotEnvCfg",
+        "play_env_cfg_entry_point": f"{__name__}.velocity_4l_env_cfg:RobotPlayEnvCfg",
         "rsl_rl_cfg_entry_point": f"unitree_rl_lab.tasks.locomotion.agents.rsl_rl_ppo_cfg:BasePPORunnerCfg",
     },
 )
